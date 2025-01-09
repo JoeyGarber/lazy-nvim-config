5df3af9e7085b799449f4448fecf6713004e42e0
Joey <joseph.z.garber@gmail.com>
2025-01-09 00:49:57 -0500

    Initial commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("i", "jk", "<Esc>")
+
+vim.keymap.set({ "i", "n", "x", "v" }, "s", "<Nop>")
+
+vim.keymap.set({ "i", "n", "x", "v" }, "<C-g>", "<Cmd>FzfLua live_grep<Cr>")
+vim.keymap.set({ "i", "n", "x", "v" }, "<C-f>", "<Cmd>FzfLua files<Cr>")
